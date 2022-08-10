43585c14ab92c085fb355e750cf3c4c0c561aa28
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-08-10 14:29:23 +0900

    26. アイキャッチ画像を挿入しよう①  - wp_post_thumbnail

--- app/public/wp-content/themes/myblog/single.php
+++ app/public/wp-content/themes/myblog/single.php
@@ -10,40 +10,45 @@
   <!-- Navigation -->
   <?php get_template_part('includes/header'); ?>
 
-  <?php if (have_posts()): ?>
-  <?php while (have_posts()) : the_post(); ?>
-    <!-- Page Header -->
-    <header class="masthead" style="background-image: url('img/post-bg.jpg')">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <div class="post-heading">
-              <h1><?php the_title(); ?></h1>
-              <span class="meta">Posted by
-                <?php the_author(); ?>
-                on <?php the_date(); ?></span>
+  <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+      <!-- Page Header -->
+      <?php
+      $id = get_post_thumbnail_id();
+      $img = wp_get_attachment_image_src($id);
+      ?>
+      <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="post-heading">
+                <h1><?php the_title(); ?></h1>
+                <span class="meta">Posted by
+                  <?php the_author(); ?>
+                  on <?php the_date(); ?></span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
 
-    <!-- Post Content -->
-    <article>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 col-md-10 mx-auto">
-            <?php the_content(); ?>
+      <!-- Post Content -->
+      <article>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(array(32, 32), array('alt' => 'アイキャッチ画像')); ?>
+              <?php the_content(); ?>
+            </div>
           </div>
         </div>
-      </div>
-    </article>
+      </article>
 
-    <hr>
-  <?php endwhile; ?>
+      <hr>
+    <?php endwhile; ?>
 
-  <!-- <?php else: ?>
+    <!-- <?php else : ?>
     <p>記事が見つかりませんでした</p> -->
   <?php endif; ?>
 
